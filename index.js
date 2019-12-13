bdb1559812098013cd9de4caeb16589fe5e9a00c
Sarah Alafif <sar1997yah@gmail.com>
2019-12-13 14:24:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,19 +60,32 @@ a.forEach(function(e,index){
     createTimeOutEvent.call(e,times[index][0][1]);
     createTimeOutEvent.call(e,times[index][1][1]);
 })
+//console.log(a)
 
 
+/*	/*
+ We're giving you this function. Take a look at it, you might see some usage	We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but	that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!	sneaky bug that we'll cover in the next few lessons!
+ As a result, the lessons for this function will pass *and* it will be available	As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!	for you to use if you need it!
+ */	*/
 
 
 let allWagesFor = function () {	let allWagesFor = function () {
-let eligibleDates = this.timeInEvents.map(function (e) {let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date })	  
- let payable = eligibleDates.reduce(function (memo, d) {	    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)	      
+
+    let eligibleDates = this.timeInEvents.map(function (e) {	    let eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date	        return e.date
+    })	    })
+
+    let payable = eligibleDates.reduce(function (memo, d) {	    let payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)	        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!	    }.bind(this),0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     
-    return payable	 
-
+    return payable	    return payable
+} 	}
+console.log(allWagesFor.call(cRecord))
 //Given an array of multiple employees
 function calculatePayroll(arr){
     let total=0;
@@ -141,6 +154,7 @@ for(let i=0;i<allRecords.length;i++){
 
 
 }
+//
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
